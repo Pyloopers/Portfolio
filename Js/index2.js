e317dc343874da554ba9708994105bc884fcab73
Amit Kumar <87274817+Pyloopers@users.noreply.github.com>
2023-08-22 11:11:48 +0530

    Removed Js

--- Js/index2.js
+++
@@ -1,9 +0,0 @@
-
-function openPDF(url) {
-    var newWindow = window.open(url, '_blank');
-    newWindow.document.addEventListener('load', function() {
-      newWindow.location.href = null;
-    });
-  }
-  <a href="" onclick="openPDF(this.href)">View Certificate</a>
-  
